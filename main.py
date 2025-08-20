2da3de3b22235f1aa5f14cb0515c035bffffd001
ParsaAbasnezhad <parsaabasy8@gmail.com>
2025-08-19 19:09:19 -0700

    Add database

--- main.py
+++ main.py
@@ -1,41 +1,27 @@
-from fastapi import FastAPI, Path, Query, HTTPException
+from fastapi import FastAPI, Path, Query, HTTPException, Depends
 from uuid import UUID
 from typing import Annotated
+from pydantic import BaseModel, Field
 
-from pydantic import BaseModel, Field, HttpUrl
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+import models
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
 
 app = FastAPI()
 
 
-# class Image(BaseModel):
-#     url: HttpUrl
-#     title: str
-#
-#
-# class Item(BaseModel):
-#     title: str = Annotated[
-#         str, Field(min_length=1, max_length=30, title='name item', description='this is a item in form')]
-#     description: str
-#     price: float
-#     tax: float | None = None
-#     image: set[Image] = set()
-#
-#
-# class Items(BaseModel):
-#     items: list[Item]
-#     body: str
-#
-#
-# @app.post("/items")
-# async def create_item(items: Items):
-#     return items
-#
-#
-
-
 class Book(BaseModel):
-    id: UUID
     title: str = Field(min_length=1, max_length=50)
+    author: str = Field(min_length=1, max_length=50)
     description: str = Field(min_length=1, max_length=500)
     rating: int = Field(ge=0, le=100)
 
@@ -44,41 +30,51 @@ BOOKS = []
 
 
 @app.get("/")
-def read_api():
-    return BOOKS
+def read_api(db: Session = Depends(get_db)):
+    return db.query(models.Books).all()
 
 
 @app.post("/")
-def create_api(book: Book):
-    BOOKS.append(book)
+def create_api(book: Book, db: Session = Depends(get_db)):
+    book_model = models.Books()
+    book_model.title = book.title
+    book_model.author = book.author
+    book_model.description = book.description
+    book_model.rating = book.rating
+    db.add(book_model)
+    db.commit()
+
     return book
 
 
 @app.put('/{book_id}')
-def update_api(book_id: UUID, book: Book):
-    counter = 0
-    for x in BOOKS:
-        counter += 1
-        if x.id == book_id:
-            BOOKS[counter - 1] = book
-            return book
-    raise HTTPException(
-        status_code=404,
-        detail="Book not found"
-    )
+def update_api(book_id: int, book: Book, db: Session = Depends(get_db)):
+    book_model = db.query(models.Books).filter(models.Books.id == book_id).first()
 
-
-@app.delete('/{book_id}')
-def delete_api(book_id: UUID):
-    counter = 0
-
-    for x in BOOKS:
-        counter += 1
-
-        if x.id == book_id:
-            del BOOKS[counter - 1]
-            return f"Book {book_id} deleted"
+    if book_model is None:
         raise HTTPException(
             status_code=404,
             detail="Book not found"
         )
+    book_model = models.Books()
+    book_model.title = book.title
+    book_model.author = book.author
+    book_model.description = book.description
+    book_model.rating = book.rating
+    db.add(book_model)
+    db.commit()
+
+    return book
+
+
+@app.delete('/{book_id}')
+def delete_api(book_id: int, db: Session = Depends(get_db)):
+    book_model = db.query(models.Books).filter(models.Books.id == book_id).first()
+
+    if book_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Book not found"
+        )
+    db.query(models.Books).filter(models.Books.id == book_id).delete()
+    db.commit()

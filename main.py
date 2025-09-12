2cfdb31f8b3f6a1a4274cd35cb13475272d81861
ParsaAbasnezhad <parsaabasy8@gmail.com>
2025-09-11 20:55:45 -0700

    Add API Item

--- main.py
+++ main.py
@@ -1,188 +1,82 @@
-from fastapi import FastAPI, Path, Query, HTTPException, Depends, Response, Cookie
-from uuid import UUID
-from typing import Annotated, List
-from pydantic import BaseModel, Field, EmailStr
-# from database import engine, SessionLocal
-from sqlalchemy.orm import Session
-import models
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel, Field, field_validator
+from typing import Optional, Annotated
 
-# def get_db():
-#     try:
-#         db = SessionLocal()
-#         yield db
-#     finally:
-#         db.close()
-#
-#
+
+class Item(BaseModel):
+    id: int
+    name: str = Field(..., min_length=3, max_length=50)
+    category: Optional[str] = Field(None, min_length=3, max_length=50)
+    price: float = Field(..., gt=0)
+
+    @field_validator("price", mode='before')
+    @classmethod
+    def price_must_be_positive(cls, value) -> int:
+        if value <= 0:
+            raise ValueError("Price must be positive")
+        return value
+
+    @field_validator("category")
+    @classmethod
+    def category_must_be_string(cls, value) -> str:
+        if value is not None and not isinstance(value, str):
+            raise ValueError("Category must be a string")
+        return value
+
+
+fake_db = [
+    Item(id=1, name="Item 1", category="Category 1", price=10),
+    Item(id=2, name="Item 2", category="Category 2", price=20),
+    Item(id=3, name="Item 3", category="Category 3", price=30),
+]
+
 app = FastAPI()
 
 
-#
-#
-#
-# class Book(BaseModel):
-#     title: str = Field(min_length=1, max_length=50)
-#     author: str = Field(min_length=1, max_length=50)
-#     description: str = Field(min_length=1, max_length=500)
-#     rating: int = Field(ge=0, le=100)
-#
-#
-# BOOKS = []
-#
-#
-# @app.get("/")
-# def read_api(db: Session = Depends(get_db)):
-#     return db.query(models.Books).all()
-#
-#
-# @app.post("/")
-# def create_api(book: Book, db: Session = Depends(get_db)):
-#     book_model = models.Books()
-#     book_model.title = book.title
-#     book_model.author = book.author
-#     book_model.description = book.description
-#     book_model.rating = book.rating
-#     db.add(book_model)
-#     db.commit()
-#
-#     return book
-#
-#
-# @app.put('/{book_id}')
-# def update_api(book_id: int, book: Book, db: Session = Depends(get_db)):
-#     book_model = db.query(models.Books).filter(models.Books.id == book_id).first()
-#
-#     if book_model is None:
-#         raise HTTPException(
-#             status_code=404,
-#             detail="Book not found"
-#         )
-#     book_model = models.Books()
-#     book_model.title = book.title
-#     book_model.author = book.author
-#     book_model.description = book.description
-#     book_model.rating = book.rating
-#     db.add(book_model)
-#     db.commit()
-#
-#     return book
-#
-#
-# @app.delete('/{book_id}')
-# def delete_api(book_id: int, db: Session = Depends(get_db)):
-#     book_model = db.query(models.Books).filter(models.Books.id == book_id).first()
-#
-#     if book_model is None:
-#         raise HTTPException(
-#             status_code=404,
-#             detail="Book not found"
-#         )
-#     db.query(models.Books).filter(models.Books.id == book_id).delete()
-#     db.commit()
-#
-#
-#
-#
-# # Login User
-#
-# class UserBase(BaseModel):
-#     username: str = Field(min_length=1, max_length=50)
-#     email: EmailStr
-#     full_name: str | None = None
-#
-#
-# class UserIn(UserBase):
-#     password: str
-#
-#
-# class UserOut(UserBase):
-#     pass
-#
-#
-# class UserInDB(UserBase):
-#     hashed_password: str
-#
-#
-# def fake_password_hasher(raw_password: str):
-#     return 'passwordid' + raw_password
-#
-#
-# def fake_save_user(user_in: UserIn):
-#     hasher_password = fake_password_hasher(user_in.password)
-#     user_in_db = UserInDB(**user_in.dict(),
-#                           hashed_password=hasher_password)
-#     print('User saved')
-#     return user_in_db
-#
-#
-# @app.post("/user/", response_model=UserOut)
-# async def create_user(user_in: UserIn):
-#     user_saved = fake_save_user(user_in)
-#     return user_saved
-#
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+@app.get("/items/{item_id}",
+         response_model=Item,
+         status_code=200
+    , tags=["Items"],
+         summary="Read an item",
+         description="Read an item by ID")
+def read_item(item_id: int):
+    if fake_db[item_id] is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return fake_db[item_id]
 
 
-@app.get("/items/")
-async def read_items() -> list[Item]:
-    return [
-        Item(name='parsa', price=42.3),
-        Item(name='pedro', price=5.5),
-        Item(name='maria', price=5.5),
-    ]
-
-
-@app.post("/items/")
-async def create_item(item: Item) -> Item:
+@app.post("/items/", response_model=Item, status_code=200, tags=["Items"], summary="Post an item",
+          description="Post an item by ID")
+async def create_item(item: Item):
+    fake_db.append(item)
     return item
 
 
-from typing import Any
-
-
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item) -> Any:
+@app.put("/items/{item_id}", response_model=Item, status_code=200, tags=["Items"], summary="put an item",
+         description="put an item by ID")
+async def update_item(item_id: int, item: Item):
+    if fake_db[item_id] is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    fake_db[item_id] = item
     return item
 
 
-@app.get("/items/", response_model=list[Item])
-async def read_items() -> Any:
-    return [
-        {'name': "laptob", 'price': 1.00, 'tax': 1.00},
-        {'name': "laptob", 'price': 1.00, 'tax': 1.00}
-    ]
+@app.delete("/items/{item_id}", response_model=Item, status_code=200, tags=["Items"], summary="delete an item",
+            description="delete an item by ID")
+async def delete_item(item_id: int):
+    if fake_db[item_id] is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    fake_db.pop(item_id)
+    return {"message": "Item deleted"}
 
 
-@app.post('/login')
-def login(response: Response):
-    response.set_cookie(
-        key='username',
-        value='admin',
-        httponly=True,
-        samesite='strict',
-        secure=True,
-        max_age=86400,
-        expires=86400,
-        path='/',
-    )
-    return {'message':"login success" }
+async def common_parameters(q: str | None = None, skip: int | None = None):
+    return {
+        "q": q,
+        "skip": skip,
+    }
 
 
-@app.post('/logout')
-def logout(response: Response):
-    response.delete_cookie(
-        key='username',
-    )
-    return {'message':"logout success" }
-
-@app.get("/user")
-def get_user(session_id: str | None = Cookie(None)):
-    if session_id:
-        return {"massage": 'login user'}
-    else:
-        return {"massage": 'no login user'}
+@app.get('/products/')
+async def read_item(common: Annotated[dict, Depends(common_parameters)])
+    return common
